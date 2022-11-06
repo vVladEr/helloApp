7644f9bd74700705ff651da1311e72886266e018
black <black@example.com>
2022-11-06 16:56:45 +0500

    repair

--- helloappBlack/helloappBlack/Program.cs
+++ helloappBlack/helloappBlack/Program.cs
@@ -1,6 +1,12 @@
 Console.WriteLine("Hello everyone");
+GreetBlack();
 
 static void GreetBlack() 
 {
     Console.WriteLine("hello black");
+}
+
+static void GreetWhite()
+{
+    Console.WriteLine("hello white");
 }
